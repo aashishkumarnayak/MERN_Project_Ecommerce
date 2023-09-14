d2aec3827166463e8ae4021a8400b8c6aa9d6830
Aashish Kumar Nayak <kumaraashish57627@gmail.com>
2023-09-14 23:53:33 +0530

    base 64 error

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(fileUpload());
+
+
 
 // Route Imports
 const product = require("./routes/productRoute");
